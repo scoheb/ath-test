044289d1aa79f4f2f71efb8ae9ed783b7aa29bf4
Scott Hebert <shebert@redhat.com>
2016-04-25 09:09:43 -0400

    example

---
+++ src/test/java/com/redhat/ATHTest.java
@@ -0,0 +1,12 @@
+package com.redhat;
+
+import org.jenkinsci.test.acceptance.junit.AbstractJUnitTest;
+import org.junit.Test;
+
+public class ATHTest extends AbstractJUnitTest {
+
+    @Test
+    public void noTestHere() {
+        
+    }
+}
